ed5f6461322404dda17f9c5cbe721bccaed7da70
Kirill Fomichev <fanatid@ya.ru>
2023-07-28 16:47:42 -0400

    backport

--- build.rs
+++ build.rs
@@ -6,7 +6,10 @@ use {
 
 fn main() -> anyhow::Result<()> {
     // Proto
-    prost_build::compile_protos(&["proto/event.proto"], &["proto/"])?;
+    let mut config = prost_build::Config::new();
+    config.boxed(".blockdaemon.solana.accountsdb_plugin_kafka.types.MessageWrapper");
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    config.compile_protos(&["proto/event.proto"], &["proto/"])?;
 
     // Version metrics
     vergen(Config::default())?;
