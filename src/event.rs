e44d2092237f6b455613bff71dcc81c2501156d4
Kirill Fomichev <fanatid@ya.ru>
2025-03-06 11:19:59 -0500

    fix conv

--- src/event.rs
+++ src/event.rs
@@ -12,23 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use agave_geyser_plugin_interface::geyser_plugin_interface::SlotStatus as PluginSlotStatus;
-
 include!(concat!(
     env!("OUT_DIR"),
     "/blockdaemon.solana.accountsdb_plugin_kafka.types.rs"
 ));
-
-// impl From<PluginSlotStatus> for SlotStatus {
-//     fn from(other: PluginSlotStatus) -> Self {
-//         match other {
-//             PluginSlotStatus::Processed => SlotStatus::Processed,
-//             PluginSlotStatus::Rooted => SlotStatus::Rooted,
-//             PluginSlotStatus::Confirmed => SlotStatus::Confirmed,
-//             PluginSlotStatus::FirstShredReceived => todo!(),
-//             PluginSlotStatus::Completed => todo!(),
-//             PluginSlotStatus::CreatedBank => todo!(),
-//             PluginSlotStatus::Dead(_) => todo!(),
-//         }
-//     }
-// }
